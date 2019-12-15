7d241b261e726bdb0f730770741f3ede53626b8e
GospodiLera <58639443+GospodiLera@users.noreply.github.com>
2019-12-15 12:02:43 +0200

    Add files via upload
    рандомное заполнение списка

--- GospodinovaLera/5.1/Main.java
+++ GospodinovaLera/5.1/Main.java
@@ -1,16 +1,17 @@
-//заобратное заполнение списка
-
 import java.util.*;
 import java.io.*;
-
+import java.io.IOException;
 public class Main {
     public static void main(String[]args) {
         int temp = 0;
         long time_add_a = 0, time_get_a= 0, time_delete_a = 0, start, end;
         Set<Integer> a = new HashSet<Integer>();
-        for (int i = 1000; i >= 0; i--)
-            a.add(i+1);
-        temp = 186;
+        for (int i = 0; i < 1150; i++)
+        {
+            temp = (int)(temp + Math.random()*100+1)*(int)Math.pow(-1,(int)(Math.random()*100));
+            a.add(temp);
+        }
+        temp = -4;
 
         start = System.nanoTime();
         a.add(temp);
@@ -29,9 +30,13 @@ public class Main {
         System.out.println(time_add_a + " " + time_get_a + " " + time_delete_a);
 
         TreeSet<Integer> t = new TreeSet<Integer>();
-        for(int i =1000; i >= 0; i--)
-            t.add(i+1);
+        for (int i = 0; i < 1100; i++)
+        {
+            temp = (int)(temp + Math.random()*100+1)*(int)Math.pow(-1,(int)(Math.random()*100));
+            t.add(temp);
+        }
         long time_add_t = 0, time_get_t = 0, time_delete_t = 0, start_t, end_t;
+        temp = -2;
         start = System.nanoTime();
         t.add(temp);
         end = System.nanoTime();
@@ -49,9 +54,13 @@ public class Main {
         System.out.println(time_add_t + " " + time_get_t + " " + time_delete_t);
 
         ArrayList<Integer> l = new ArrayList<Integer>();
-        for(int i = 1000; i >= 0; i--)
-            l.add(i+1);
+        for (int i = 0; i < 1000; i++)
+        {
+            temp = (int)(temp + Math.random()*100+1)*(int)Math.pow(-1,(int)(Math.random()*100));
+            l.add(temp);
+        }
         long time_add_l = 0, time_get_l = 0, time_delete_l = 0, start_l, end_l;
+        temp = 4;
         start = System.nanoTime();
         l.add(temp);
         end = System.nanoTime();
@@ -69,9 +78,13 @@ public class Main {
         System.out.println(time_add_l + " " + time_get_l + " " + time_delete_l);
 
         LinkedList<Integer> ll = new LinkedList<Integer>();
-        for(int i = 1000; i >= 0; i--)
-            ll.add(i+1);
+        for (int i = 0; i < 1000; i++)
+        {
+            temp = (int)(temp + Math.random()*100+1)*(int)Math.pow(-1,(int)(Math.random()*100));
+            ll.add(temp);
+        }
         long time_add_ll = 0, time_get_ll = 0, time_delete_ll = 0, start_ll, end_ll;
+        temp = 5;
         start = System.nanoTime();
         ll.add(temp);
         end = System.nanoTime();
@@ -87,14 +100,23 @@ public class Main {
         end = System.nanoTime();
         time_delete_ll = end - start;
         System.out.println(time_add_ll + " " + time_get_ll + " " + time_delete_ll);
+        System.out.println(a.size());
+        System.out.println(t.size());
+        System.out.println(l.size());
+        System.out.println(ll.size());
+
     }
 }
-/*
-Имеем:
-1392 20870 10202
-2782 97391 18087
-928 250898 11130
-928 90899 14840
-Выходит, что TreeSet хуже всех справляется с добавлением и удалением элемента. Хуже всех в поиске элемента справлется ArrayList.
-LinkedList i ArrayList лучше всех добавляют элемент, а вот HashSet быстрее всех справляется с поиском и удалением элемента.
-*/
+
+/*Проверяем время выполнения добавления, удаления и поиска элементов для коллекций, заполненых рандомно.
+Так как коллекция сет предусматривает только попарно разные элементы, то я увеличила количество элементов в коллекции. В итоге имеем:
+1391 17623 9275
+3711 19942 21797
+464 941912 20869
+928 90435 13913
+1008
+1022
+1000
+1000
+
+То есть TreeSet заметно уступает по времени добавления и удаления элемента, а ArrayList проигрывает по времени поиска элемента.*/
